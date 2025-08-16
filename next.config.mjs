04f15514ba5a2109cbb9b9bf48208c2d21bee9d6
Srikanth, Phani <Phani.Srikanth@netapp.com>
2025-08-17 02:53:37 +0530

    fix html

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/vibe-dat-creativity-app' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/vibe-dat-creativity-app/' : '',
   images: {
     unoptimized: true
   }
